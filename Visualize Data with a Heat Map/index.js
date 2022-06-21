829350bbc7696ccddc7c2458c893790ca28f2d90
V123in4 <vinconloren@gmail.com>
2022-06-22 00:52:10 +0700

    Added scales to the script

--- Visualize Data with a Heat Map/index.js
+++ Visualize Data with a Heat Map/index.js
@@ -3,6 +3,47 @@ req.open('GET', 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReference
 req.send();
 req.onload = () => {
     const rawdata = JSON.parse(req.responseText);
-    const data = rawdata.map(d => [new Date(`Jan 01, ${d['Year']} 00:00:00`), new Date(`Jan 01, 0000 00:${d['Time']}`)]);
-    
+    const dataset = rawdata;
+
+    const monthNumToString = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December",
+    }
+
+    const svgHeight = 500, svgWidth = 900;
+    const svgPadding = 30;
+
+    const svg = d3.select("body")
+        .append("svg")
+        .attr("height", svgHeight)
+        .attr("width", svgWidth);
+
+    xScale = d3.scaleLinear()
+        .domain([d3.min(dataset.monthlyVariance.map(d => d.year)), d3.max(dataset.monthlyVariance.map(d => d.year))])
+        .range([svgPadding, svgWidth - svgPadding]);
+
+    yScale = d3.scaleLinear()
+        .domain([d3.max(dataset.monthlyVariance.map(d => d.month)), d3.min(dataset.monthlyVariance.map(d => d.month))])
+        .range([svgHeight, svgPadding])
+
+    console.log(dataset["monthlyVariance"].map(d => d["month"]));
+
+    // svg.selectAll("rect")
+    //     .data(dataset)
+    //     .enter()
+    //     .append("rect")
+    //     .attr()
+    //     .attr()
+
+
 };
